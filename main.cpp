528501dbdb8a92ee70135eb9fc0c8de2e0b97dce
Gostrondude <42420087+Gostrondude@users.noreply.github.com>
2019-02-26 13:18:30 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,17 +6,33 @@
 
 using namespace std;
 
+int Hundred = 100;
+
 int input1;
 int input2;
 int type;
 
-const char* helpCommands =
-"1. Addition\n"
-"2. Subtraction\n"
-"3. Multiplication\n"
-"4. Division\n"
-"5. Help\n"
+namespace CommandStuff{
+const char* HelpCommands =
+"Current List Of Commands:\n"
+"1 = Addition\n"
+"2 =  Subtraction\n"
+"3 = Multiplication\n"
+"4 = Division\n"
+"5 = Decimal To Percent\n"
+"6 = Help\n"
+"7 = Descriptions Of Commands\n"
 ;
+
+const char* Descriptions = 
+"Addition - Adds 2 Numbers Together\n"
+"Subtraction - Subtracts 2 Numbers\n"
+"Multiplication - Multiplys 2 Numbers\n"
+"Division - Divides 2 Numbers\n"
+"Decimal To Percent - Converts An Decimal To A Percent"
+"Help - Shows A List Of Commands\n"
+;
+}
 
 namespace Calculator{
   void Add(){
@@ -31,40 +47,50 @@ namespace Calculator{
   void Divide(){
     std::cout << "Answer: " << input1 / input2 << "\n";
   }
+  void Decimal2Percent(){
+    std::cout << "Answer: " << input1 * Hundred << "% \n";
+  }
+  void Percent2Decimal(){
+    std::cout << "Answer: " << input1 / Hundred << "\n";
+  }
 }
 
 int main(){
 
-  std::cout << "Which Of The Following Would You Like To Do: \n 1. Addition \n 2. Subtraction  \n 3. Multiplication  \n 4. Division\n 5. Help\n";
+  std::cout << CommandStuff::HelpCommands;
+
   while (true){
     cin >> type;
     if (type == 1){
     std::cout << "Now Put In 2 Numbers \n";
     cin >> input1 >> input2;
     Calculator::Add();
-    std::cout << "Choose What You Would Like To Do Next (Say '5' For The List Of Commands\n";
   }
   else if (type == 2){
     std::cout << "Now In 2 Numbers \n";
     cin >> input1 >> input2;
     Calculator::Subtract();
-    std::cout << "Choose What You Would Like To Do Next (Say '5' For The List Of Commands\n";
   }
   else if (type == 3){
     std::cout << "Now Put In 2 Numbers \n";
     cin >> input1 >> input2;
     Calculator::Multiply();
-    std::cout << "Choose What You Would Like To Do Next (Say '5' For The List Of Commands\n";
   }
   else if (type == 4){
     std::cout << "Now Put In 2 Numbers \n";
     cin >> input1 >> input2;
     Calculator::Divide();
-    std::cout << "Choose What You Would Like To Do Next (Say '5' For The List Of Commands\n";
   }
   else if (type == 5){
-    std::cout << helpCommands;
-    std::cout << "Choose What You Would Like To Do Next (Say '5' For The List Of Commands Again \n";
+    std::cout << "Put In The Number You Would Like To Convert From An Decimal To A Percent \n";
+    cin >> input1;
+    Calculator::Decimal2Percent();
+  }
+  else if (type == 6){
+    std::cout << CommandStuff::HelpCommands;
+  }
+  else if (type == 7){
+    std::cout << CommandStuff::Descriptions;
   }
   }
 }
